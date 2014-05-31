5d22e2b17e0b195bf9abf948b4d8ea630af22e26
phosphene <ed@cronos.net>
2014-05-30 20:24:09 -0400

    understanding speclj components spec test

--- spec/storm_test_demo/core_spec.clj
+++ spec/storm_test_demo/core_spec.clj
@@ -3,9 +3,40 @@
 
 (defn true-or-false []
   true)
+(defn equals-5? [n]
+  (= 5 n))
+
+
+
 
 (describe "truthiness"
   (it "tests if true-or-false returns true"
     (should (true-or-false))))
+
+(describe "Understanding Speclj Components"
+  (before-all
+    (println "Start testing"))
+  
+  (after-all
+    (println "Finished testing"))
+    
+  (before
+    (println "Before each test"))
+    
+  (after
+    (println "After each test"))
+
+  (it "4 plus 1 equals 5"
+    (println "During 1st Test")
+    (should (equals-5? (+ 4 1))))
+    
+  (it "4 plus 2 doesn't equal 5"
+    (println "During 2nd Test")
+    (should-not (equals-5? (+ 4 2))))
+  
+  (it "4 plus 2 equals 6"
+    (println "During 3rd Test")
+    (should= 6 (+ 4 2)))
+)
 
 (run-specs)
